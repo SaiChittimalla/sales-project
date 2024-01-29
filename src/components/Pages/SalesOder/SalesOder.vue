3146f141ccd020948e01060d5a4b86ed1418473b
caratred <saikumar.c@caratred.com>
2024-01-29 15:12:45 +0530

    changes for quotaions and fianlitems

--- src/components/Pages/SalesOder/SalesOder.vue
+++ src/components/Pages/SalesOder/SalesOder.vue
@@ -1,173 +1,189 @@
 <template>
-    <div class="container-fluid">
-      <div class="d-flex justify-content-between align-items-center mt-2 header">
-        <div class="d-flex gap-2 p-3 align-items-center">
-            <div>
-            <i class="ri-arrow-left-line  "><span class="ps-2 quotationsfs">SalesOders(199)</span></i>
-          </div>
-         
-        </div>
-        <div class="ri-search-line1">
-          <i class="ri-search-line"></i>
-        </div>
-      </div>
-      <div class="header2">
-        <div class="d-flex justify-content-between align-items-center buttonsall">
-          <ul class="d-flex flex-wrap list-unstyled justify-content-between list-group  flex-row gap-1 ">
-            <div></div>
-            <li class=" list-group-item active btn1">All</li>
-            <li class=" list-group-item btn2">Draft</li> 
-            <li class=" list-group-item btn2">Saved</li>
-            <li class=" list-group-item btn2">
-              <select class="border-0">
-                <option class="border-0">This Month</option>
-              </select>
-            </li>
-          </ul>
+  <nav class="navbar header align-items-center">
+    <div class="container">
+      <div class="d-flex gap-2 p-2 align-items-center">
+        <div>
+          <i class="ri-arrow-left-line"
+            ><span class="ps-2 quotationsfs"
+              >SalesOrders &nbsp;({{ totalQuotations }})</span
+            ></i
+          >
         </div>
       </div>
-      <div class="content">
-        <div class="row">
-          <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-4">
-            <div class="card card1 h-100">
-              <div
-                class="d-flex justify-content-between p-2 align-items-baseline"
-                style="border-bottom: 1px solid #eeeeee"
-              >
-                <div class="d-flex gap-3 align-items-center">
-                  <div class="d-flex justify-content-center ri-file-edit-line1">
-                    <i class="ri-file-edit-line"></i>
-                  </div>
-                  <div>
-                    <p style="font-size: 13px" class="align-items-center mt-3">
-                      SAL-QTN-2023-00001 <br /><span
-                        class="text-muted"
-                        style="font-size: 11px"
-                        >12 Dec</span
-                      >
-                    </p>
-                  </div>
-                </div>
-                <div>
-                  <button class="savedbutton">Saved</button>
-                </div>
-              </div>
-              <div class="p-3">
-                <h6 class="" style="font-size: 13px">Anasuya karthula</h6>
-                <p style="font-size: 11px; color: #3b43f9">Customer</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-4">
-            <div class="card card1 h-100">
-              <div
-                class="d-flex justify-content-between p-3 align-items-baseline"
-                style="border-bottom: 1px solid #eeeeee"
-              >
-                <div class="d-flex gap-3 align-items-center">
-                  <div class="d-flex justify-content-center ri-file-edit-line1">
-                    <i class="ri-file-edit-line"></i>
-                  </div>
-                  <div>
-                    <p style="font-size: 13px" class="align-items-center mt-3">
-                      SAL-QTN-2023-00001 <br /><span
-                        class="text-muted"
-                        style="font-size: 11px"
-                        >12 Dec</span
-                      >
-                    </p>
-                  </div>
-                </div>
-                <div>
-                  <button class="savedbutton">Saved</button>
-                </div>
-              </div>
-              <div class="p-3">
-                <h6 class="" style="font-size: 13px">Anasuya karthula</h6>
-                <p style="font-size: 11px; color: #3b43f9">Lead</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 mb-4">
-            <div class="card card1 h-100">
-              <div
-                class="d-flex justify-content-between p-3 align-items-baseline"
-                style="border-bottom: 1px solid #eeeeee"
-              >
-                <div class="d-flex gap-3 align-items-center">
-                  <div class="d-flex justify-content-center ri-file-edit-line1">
-                    <i class="ri-file-edit-line"></i>
-                  </div>
-                  <div>
-                    <p style="font-size: 13px" class="align-items-center mt-3">
-                      SAL-QTN-2023-00001 <br /><span
-                        class="text-muted"
-                        style="font-size: 11px"
-                        >12 Dec</span
-                      >
-                    </p>
-                  </div>
-                </div>
-                <div>
-                  <button class="draftbtn">Draft</button>
-                </div>
-              </div>
-              <div class="p-3">
-                <h6 class="" style="font-size: 13px">Anasuya karthula</h6>
-                <p style="font-size: 11px; color: #3b43f9">Customer</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div>
-         <button
-          class="btn btn-primary circle-with-plus"
-          type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasBottom"
-          aria-controls="offcanvasBottom"
-        >
-          +
-        </button>
-  
-        <!-- <div
-          class="offcanvas offcanvas-bottom h-50"
-          tabindex="-1"
-          id="offcanvasBottom"
-          aria-labelledby="offcanvasBottomLabel"
-        >
-          <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasBottomLabel"></h5>
-            <button
-              type="button"
-              class="btn-close text-reset"
-              data-bs-dismiss="offcanvas"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="offcanvas-body small">
-            <h5 class="offcanvastext text-center">
-              <b>Specify the quotation's <br />intended recipient.</b>
-            </h5>
-            <div>
-              <div class="mt-3">
-                <button class="btn btncustomer w-100">To Customer</button>
-              </div>
-              <div class="mt-3">
-                <button class="btn btncustomer w-100">To Lead</button>
-              </div>
-            </div>
-          </div>
-        </div>  -->
+      <div class="ri-search-line1">
+        <i class="ri-search-line"></i>
       </div>
     </div>
-  </template>
+  </nav>
+  <div class="container">
+    <div class="header2">
+      <div class="d-flex justify-content-between align-items-center buttonsall">
+        <ul class="d-flex flex-wrap list-unstyled list-group flex-row gap-1">
+          <div></div>
+          <li
+            class="list-group-item btn2"
+            :class="{ active: activeFilter === 'All' }"
+            @click="setFilter('All')"
+          >
+            All
+          </li>
+          <li
+            class="list-group-item btn2"
+            :class="{ active: activeFilter === 'Draft' }"
+            @click="setFilter('Draft')"
+          >
+            Draft
+          </li>
+          <li
+            class="list-group-item btn2"
+            :class="{ active: activeFilter === 'Completed' }"
+            @click="setFilter('Completed')"
+          >
+            Completed
+          </li>
+          <li class="list-group-item btn2" @click="setFilter('This Month')">
+            <select class="border-0">
+              <option class="border-0">This Month</option>
+            </select>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="content">
+      <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-4">
+          <div
+            class="card card1 mb-4"
+            v-for="(sales, index) in filteredQuotations"
+            :key="index"
+          >
+            <div
+              class="d-flex justify-content-between p-2 align-items-baseline"
+              style="border-bottom: 1px solid #eeeeee"
+            >
+              <div class="d-flex gap-3 align-items-center">
+                <div class="d-flex justify-content-center ri-file-edit-line1">
+                  <i class="ri-file-edit-line"></i>
+                </div>
+                <div>
+                  <p style="font-size: 13px" class="align-items-center mt-3">
+                    {{ sales.name }}<br /><span
+                      class="text-muted"
+                      style="font-size: 11px"
+                      >{{ sales.transaction_date }}</span
+                    >
+                  </p>
+                </div>
+              </div>
+              <div>
+                <button class="savedbutton">{{ sales.status }}</button>
+              </div>
+            </div>
+            <div class="p-3">
+              <h6 class="" style="font-size: 13px">
+                {{ sales.customer_name }}
+              </h6>
+              <p style="font-size: 11px; color: #3b43f9">Customer</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <button
+        class="btn btn-primary circle-with-plus"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasBottom"
+        aria-controls="offcanvasBottom"
+      >
+        +
+      </button>
+    </div>
+  </div>
+</template>
+  <script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      data: [],
+      saleOrders: [],
+      filterType: "All",
+      activeFilter: "All",
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  computed: {
+    totalQuotations() {
+      return this.filteredQuotations.length;
+    },
+    filteredQuotations() {
+      if (this.filterType === "All") {
+        return this.saleOrders;
+      } else {
+        // Filter quotations based on the selected type
+        return this.saleOrders.filter(
+          (sales) => sales.status === this.filterType
+        );
+      }
+    },
+  },
+  methods: {
+    setFilter(type) {
+      this.activeFilter = type;
+      this.filterType = type;
+    },
+    fetchData() {
+      this.loading = true;
+      axios
+        .get(
+          "http://192.168.1.177:8000/api/resource/Sales%20Order?fields=[%22*%22]",
+          {
+            params: {
+              fields: JSON.stringify(["*"]),
+            },
+          }
+        )
+        .then((response) => {
+          this.saleOrders = JSON.parse(JSON.stringify(response.data.data));
+          console.log(this.saleOrders);
+        })
+        .catch((error) => {
+          console.error(error.message);
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+    //  fetchData() {
+    //   let queryParams = { filters: [] };
+    //   // queryParams.filters.push(["audit_record_id", "=", this.$route.query.id]);
+    //   queryParams.filters.push(["section", "=", "Main LT Panel"]);
+    //   queryParams.filters = JSON.stringify(queryParams.filters);
+    //   queryParams.fields = JSON.stringify(["*"]);
+    //   console.log("name:", this.$route.query.id);
+    //   console.log("tab name :" + this.fetchData);
+    //   axios
+    //     .get(
+    //       "http://192.168.1.177:8000/api/resource",
+    //       {
+    //         params: queryParams,
+    //       }
+    //     )
+    //     .then((response) => {
+    //       this.data = JSON.parse(JSON.stringify(response.data.data));
+    //     })
+    //     .catch((error) => {
+    //       console.error(error);
+    //     });
+    // },
+  },
+};
+</script>
   <style scoped>
-/* .offcanvas-bottom { 
-  height: 30%;
-} */
-
 .draftbtn {
   border-radius: 4px;
   border: 0.7px solid #999;
@@ -184,19 +200,7 @@
   font-weight: 500;
   line-height: normal;
 }
-.btncustomer {
-  border-radius: 30px;
-  background: #3b43f9;
-  color: white;
-}
-.offcanvastext {
-  color: #111;
-  font-family: Montserrat;
-  font-size: 28px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: normal;
-}
+
 .ri-search-line1 {
   width: 32px;
   height: 32px;
@@ -232,9 +236,11 @@
   position: fixed;
   /* top: 100%; */
   bottom: 10%;
-  right: 6%;
+  right: 17%;
 }
-
+.buttonsall {
+  padding: 10px 10px;
+}
 .quotationsfs {
   color: #111;
   font-family: Montserrat;
@@ -264,16 +270,13 @@
   flex-shrink: 0;
 }
 .header2 {
-  
   flex-shrink: 0;
   background: #fff;
   box-shadow: 0px 11px 24px 0px rgba(0, 0, 0, 0.05);
   width: 100%;
 }
-.buttonsall {
-  padding: 10px 10px;
-}
-.active{
+
+.active {
   border-radius: 20px;
   background: #3b43f9;
   color: white;
@@ -297,11 +300,17 @@
   font-weight: 500;
   line-height: 18px; /* 150% */
   padding: 4px 15px;
- 
-  /* margin: 0px 5px; */
+
+  margin: 3px 5px;
 }
-li{
-    margin-top: 10px;
+.list-group-item + .list-group-item.active {
+  border-top-width: 1px !important;
+  background: #3b43f9;
+  color: white;
+  padding: 5px 14px;
+}
+li {
+  margin-top: 10px;
 }
 
 .content {
@@ -357,5 +366,4 @@ p {
 }
 </style>
 <script setup>
-    
 </script>
