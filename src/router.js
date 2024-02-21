b634bbcea0d67a3759bc2f4867c99eecff51a345
caratred <saikumar.c@caratred.com>
2024-02-21 16:24:41 +0530

    contactpage

--- src/router.js
+++ src/router.js
@@ -16,7 +16,7 @@ import AddCustomers from './components/Pages/clientsBuyers/Customer/AddCustomers
 import CustomerForm from './components/Pages/clientsBuyers/Customer/CustomerForm.vue'
 import NewSalesOrder from './components/Pages/pipeline/SalesOrder/NewSalesOrder.vue';
 import PracticePage from "./components/PracticePage.vue";
-import DeliveryNotes from "./components/Pages/pipeline/SalesOrder/DeliveryNotes.vue";
+
 
 const routes = [
     {
@@ -94,11 +94,7 @@ const routes = [
         path: '/NewSalesOrder',
         component: NewSalesOrder
     },
-    {
-        name: 'DeliveryNotes',
-        path: '/DeliveryNotes',
-        component: DeliveryNotes
-    },
+
 
 
 
